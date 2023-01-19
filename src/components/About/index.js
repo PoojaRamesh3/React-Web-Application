05e33f4f2f3d2fc0a29ec35a5f28f1b6d6c91968
Pooja R <pooja.ramesh@imaginelearning.com>
2023-01-19 16:45:52 +0530

    Coloration

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,40 @@
 import './index.scss'
+import React, { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 
 const About = () => {
-  return <div className="container about-page"></div>
+  const [letterClass] = useState('text-animate')
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+            idx={15}
+          />
+        </h1>
+        <p className="about-me">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam,
+          totam minima dolorem, ut odit non labore et voluptatibus esse
+          reprehenderit doloribus error rerum fugit nulla eos reiciendis sed
+          itaque eveniet?
+        </p>
+        <p className="about-me">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam,
+          totam minima dolorem, ut odit non labore et voluptatibus esse
+          reprehenderit doloribus error rerum fugit nulla eos reiciendis sed
+          itaque eveniet?
+        </p>
+        <p className="about-me">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam,
+          totam minima dolorem, ut odit non labore et voluptatibus esse
+          reprehenderit doloribus error rerum fugit nulla eos reiciendis sed
+          itaque eveniet?
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default About
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,22 @@
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import React, { useState } from 'react'
 
 const Contact = () => {
-  return <div className="container contact"></div>
+  const [letterClass] = useState('text-animate')
+  return (
+    <div className="container contact-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+            idx={15}
+          />
+        </h1>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
